4017cf023ec70fa57f717c09573578ed1312a3bf
Renato Porto <renatoafporto@gmail.com>
2020-03-25 11:14:36 -0300

    Adding a note

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,45 @@
-const getNotes = function () {
-  return 'Your notes...'
-}
+const fs = require("fs");
+const chalk = require('chalk')
 
-module.exports = getNotes
+const getNotes = function() {
+  return "Your notes...";
+};
+
+const addNote = function(title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title
+  })
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    saveNotes(notes);
+    console.log("Note added")
+  } else {
+    console.log(chalk.red("ERROR") + " Title already taken")
+  }
+
+};
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+};
