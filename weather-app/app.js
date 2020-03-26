40b157f93edef1831092451945d8eb01527ff9a5
Renato Porto <renatoafporto@gmail.com>
2020-03-26 15:28:53 -0300

    Request to geocode api

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,12 +8,18 @@ const languageOption = "lang=pt"
 
 const url = apiUrl + apiKey + "/" + location + "?" + unitOption + "&" + languageOption;
 
-request({ url: url, json: true }, (error, response) => {
-  const temperature = response.body.currently.temperature;
-  const precipProbability = response.body.currently.precipProbability;
-  console.log(response.body.daily.data[0].summary)
-  console.log("It is currently " + temperature + " degrees out.");
-  console.log("There is a " + precipProbability + "% chance of rain.");
-});
+// request({ url: url, json: true }, (error, response) => {
+//   const temperature = response.body.currently.temperature;
+//   const precipProbability = response.body.currently.precipProbability;
+//   console.log(response.body.daily.data[0].summary)
+//   console.log("It is currently " + temperature + " degrees out.");
+//   console.log("There is a " + precipProbability + "% chance of rain.");
+// });
 
-const fToC = fTemp => ((fTemp - 32) * 5) / 9;
+const geocodeURL = 
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Belo%20Horizonte.json?access_token=pk.eyJ1IjoicmFmcG9ydG8iLCJhIjoiY2s4OTJpa3pmMDJsZDNlbzFvMWwwZGVmNiJ9.Y3Ph57iW6J6oH91YVLhd2g"
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  console.log("Latitude: " + response.body.features[0].center[1])
+  console.log("Longitude: " + response.body.features[0].center[0])
+})
