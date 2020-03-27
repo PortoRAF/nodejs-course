7cefdc179892f61a358057b00b47a717d0f131af
Renato Porto <renatoafporto@gmail.com>
2020-03-27 18:29:14 -0300

    Chaining functions and getting user input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,23 @@
-const request = require("request");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("belo horizonte", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+const address = process.argv.slice(2);
 
-forecast(-19.9227, -43.9451, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+if (process.argv.length > 2) {
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+} else {
+  console.log("Please provide an address.");
+}
