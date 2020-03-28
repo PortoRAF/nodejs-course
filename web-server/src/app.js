39e92527ddaca2930efd03b166342a7be5c20279
Renato Porto <renatoafporto@gmail.com>
2020-03-28 15:09:04 -0300

    Integrate weather app into webpage project

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
 
 const app = express();
 
@@ -40,9 +42,43 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address"
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          location,
+          forecast: forecastData,
+          address: req.query.address
+        });
+      });
+    }
+  );
+});
+
+app.get("/product", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term"
+    });
+  }
+  console.log(req.query);
   res.send({
-    forecast: "Nice weather",
-    location: "Mississippi"
+    products: []
   });
 });
 
