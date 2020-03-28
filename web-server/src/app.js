19b77fc95b7922ebf7b09296cd9e971ac06703fd
Renato Porto <renatoafporto@gmail.com>
2020-03-27 23:27:59 -0300

    Added template header and footer, also 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,21 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -22,7 +33,8 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    title: "Help Page",
+    title: "Help",
+    author: "Renato Porto",
     helpText: "This is a help message"
   });
 });
@@ -31,6 +43,22 @@ app.get("/weather", (req, res) => {
   res.send({
     forecast: "Nice weather",
     location: "Mississippi"
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    author: "Renato Porto",
+    errorMessage: "Help article not found"
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    author: "Renato Porto",
+    errorMessage: "Page not found"
   });
 });
 
