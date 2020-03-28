83ad29b93149ad29e2cf879d7d49c5a0e0de69fb
Renato Porto <renatoafporto@gmail.com>
2020-03-27 22:04:24 -0300

    Migrate static template to dynamic pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,29 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    author: "Renato Porto"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    author: "Renato Porto"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    helpText: "This is a help message"
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
